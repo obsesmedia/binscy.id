7bf21868bcdf6fc03806c7c95e26903aafb958a9
obsesmedia <129799153+obsesmedia@users.noreply.github.com>
2023-12-29 14:28:41 +0700

    Add files via upload

---
+++ src/app/(pages)/aboutUs/page.tsx
@@ -0,0 +1,58 @@
+import NavbarHeader from "@/app/navbarFooter";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+export default function AboutUs() {
+  return (
+    <NavbarHeader>
+      <div className="tracking-wider">
+        <div
+          style={{
+            backgroundImage: "url(/bgAboutUs.jpg)",
+          }}
+          className="text-slate-100 text-2xl font-semibold h-52 rounded-xl overflow-hidden"
+        >
+          <div className="w-full h-full backdrop-blur-sm text-center flex items-center justify-center">
+            <h1>TENTANG KAMI</h1>
+          </div>
+        </div>
+
+        <div className="mt-8 mb-24 lg:grid grid-cols-2 ">
+          <div>
+            <h2 className="font-semibold text-xl p-6 text-center">OWNER</h2>
+            <div
+              style={{
+                backgroundImage: `url(/aboutUs.jpg)`,
+              }}
+              className="rounded-3xl m-auto overflow-hidden h-[400px] bg-center bg-cover relative max-w-xs"
+            >
+              <div className="text-white backdrop-blur-md rounded-3xl absolute bottom-0 p-4">
+                <p className="font-semibold text-xl">Rafael</p>
+                <p className="font-medium">Owner</p>
+                <p className="mt-5 text-xs font-light text-justify">
+                  Sebagai Owner, Rafael mengawasi strategi dan pelaksanaan
+                  TopupBinscy.com agar perusahaan mencapai tujuan
+                  bisnisnya.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="font-semibold text-xl p-6 text-center">ALAMAT</h2>
+            <div className="p-4 rounded-2xl max-w-sm m-auto">
+              <div className="text-secondary bg-slate-700 w-max p-3 text-2xl rounded-2xl h-max">
+                <FaMapMarkerAlt />
+              </div>
+              <div>
+                <p className="text-slate-100 text-sm font-light pt-4">
+                  Jl. Triloka ll No. 5 Rt.04 / Rw.04, Kec. Pancoran, Kab. Jakarta Selatan
+                  21123 Telepon: 089635531856
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </NavbarHeader>
+  );
+}
